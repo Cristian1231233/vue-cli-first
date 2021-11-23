d3b9689008b4565ac9ec3580ed6c95d51bb0fe15
Cristian1231233 <campagnacristian22@gmail.com>
2021-11-23 16:27:51 +0100

    commit

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+     <h1>La mia prima app con Vue CLI!</h1>
+  </header>
+   
+      
+   
+  
+</template>
+
+<script>
+export default {
+    nome: 'Header'
+    
+}
+</script>
+
+<style>
+
+</style>
